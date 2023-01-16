1d8ee9a82932c5672af35d51524779b58d1790bb
Jay Kumar Gaur <jaygaur99@gmail.com>
2023-01-16 23:26:53 +0530

    Added posts and users model, routes, controllers

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
 
 /* CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url);
@@ -38,9 +42,12 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* CONFIGURE AND SETUP MONGOOSE */
 // eslint-disable-next-line no-undef
